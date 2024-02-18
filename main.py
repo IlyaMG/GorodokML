d6f86ada397b3f8682827c9901b65df4fa317dfd
Ilya <blacklord@list.ru>
2024-02-19 00:15:59 +1000

    Модель сервиса

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import threading
+import flask
+from flask import jsonify, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = flask.Flask(__name__)
+
+status = "None"
+
+def async_training():
+    global model
+    global status
+
+@app.route("/training", methods=["GET"])
+def training():
+    global status
+    if status == "None" or status == "Сompleted":
+        thr1 = threading.Thread(target=async_training)
+        thr1.start()
+
+    return "Training started"
+
+@app.route("/status", methods=["GET"])
+def getstatus():
+    global status
+    return status
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+@app.route("/predict", methods=["POST"])
+def predict():
+    result = ""
+    return jsonify(result)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(host='192.168.10.21', port=5000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
